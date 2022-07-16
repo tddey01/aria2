a41ad3a4922725c7855a35207b88e544ad1b0dab
kn <kn@gmail.com>
2022-07-16 16:08:37 +0800

    基础提交 2次

--- logger/rotate.go
+++ logger/rotate.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"encoding/json"
 	"github.com/tddey01/aria2/daemon"
 	"io"
 	"io/ioutil"
@@ -58,6 +59,20 @@ func (hook *RotateFileHook) Fire(entry *logrus.Entry) (err error) {
 	} else {
 		entry.Logger.Out = os.Stdout
 	}
+	//[7025910 0 0 0 0 0 0 0 0 0 0 0 0 7025911 0]
+	//	json.
+
+	arr := []uint{7025910, 0, 0, 0, 0, 0, 0}
+	s := T_arr{Arr: arr}
+
+	b, _ = json.Marshal(s)
+
+	var newArr T_arr
+	json.Unmarshal(b, &newArr)
 
 	return nil
+}
+
+type T_arr struct {
+	Arr []uint `json:"arr"`
 }
