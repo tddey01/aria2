2c0a7060255fdfb96ba4ac3487e88ecd67b0f604
kn <kn@gmail.com>
2022-07-20 18:20:30 +0800

    基础提交 3次

--- model/filswan.go
+++ model/filswan.go
@@ -94,7 +94,8 @@ type Dw struct {
 	Downloading string `gorm:"column:downloading" json:"downloading"`
 	Downloaded  string `gorm:"column:downloaded" json:"downloaded"`
 }
-func GetCount() ( ret *Dw, err error) {
+
+func GetCount() (ret []*Dw, err error) {
 	sqlx := `select sum(skt) as downloading  ,sum(stk) as downloaded  from  ( select  count(data_cid) as  skt ,0 as stk  from  filswan where  locked=1
     union  all select  0 as skt , count(data_cid) as stk  from filswan where file_active=2 )a`
 	log.Debug(sqlx)
--- service/wetch.go
+++ service/wetch.go
@@ -9,7 +9,7 @@ import (
 )
 
 func BlockStartNewTotal3() {
-	spec := "01, 18, *, *, *, *" // 每天23 点 01 分
+	spec := "01, 23, *, *, *, *" // 每天23 点 01 分
 	c := cron.New()
 	if err := c.AddFunc(spec, BlockStartNew); err != nil {
 		log.Error("当天出块 统计失败  func error:", err.Error())
@@ -32,10 +32,10 @@ func BlockTotalCount() (err error) {
 	str := fmt.Sprintf(" %s", utils.TimeHMS())
 	datacount, err := model.GetCount()
 	if err != nil {
-		return
+		return err
 	}
 
-	str += fmt.Sprintf("\n下载中 >>>>：%s \n下载完成>>: %s ", datacount.Downloading, datacount.Downloaded) //节点：f080468  有效算力: 6.432 PiB  今日块: 3  24h幸运值：80.00% 3日内块：2
+	str += fmt.Sprintf("\n下载中 >>>>：%s \n下载完成>>: %s ", datacount[0].Downloading, datacount[0].Downloaded) //节点：f080468  有效算力: 6.432 PiB  今日块: 3  24h幸运值：80.00% 3日内块：2
 	log.Info(" 发消息 企业微信你")
 	restp := &Msg{
 		Touser:  "@all",
