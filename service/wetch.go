9b3ebd61e699edfc85225b7eae033cfc917d0a61
kn <kn@gmail.com>
2022-07-20 18:24:00 +0800

    基础提交 3次

--- service/wetch.go
+++ service/wetch.go
@@ -9,7 +9,7 @@ import (
 )
 
 func BlockStartNewTotal3() {
-	spec := "01, 23, *, *, *, *" // 每天23 点 01 分
+	spec := "01, 01, *, *, *, *" // 每天23 点 01 分
 	c := cron.New()
 	if err := c.AddFunc(spec, BlockStartNew); err != nil {
 		log.Error("当天出块 统计失败  func error:", err.Error())
@@ -35,7 +35,7 @@ func BlockTotalCount() (err error) {
 		return err
 	}
 
-	str += fmt.Sprintf("\n下载中 >>>>：%s \n下载完成>>: %s ", datacount[0].Downloading, datacount[0].Downloaded) //节点：f080468  有效算力: 6.432 PiB  今日块: 3  24h幸运值：80.00% 3日内块：2
+	str += fmt.Sprintf("\n下载中 >>>>：%s \n下载完成 >>: %s ", datacount[0].Downloading, datacount[0].Downloaded) //节点：f080468  有效算力: 6.432 PiB  今日块: 3  24h幸运值：80.00% 3日内块：2
 	log.Info(" 发消息 企业微信你")
 	restp := &Msg{
 		Touser:  "@all",
