4d9dd93074aac6a6bba4d12cfb9792ffed6967f6
kn <kn@gmail.com>
2022-07-21 18:27:11 +0800

    基础提交 8次

--- model/filswan.go
+++ model/filswan.go
@@ -20,7 +20,7 @@ type FilSwan struct {
 }
 
 func GetAll() (ret []*FilSwan, err error) {
-	sqlx := `select  * from  filswan where file_active=0  limit 0,1`
+	sqlx := `select  * from  filswan where file_active=0  limit 0,5`
 	log.Debug(sqlx)
 	if err = orm.Eloquent.Raw(sqlx).Scan(&ret).Error; err != nil {
 		return
@@ -45,16 +45,6 @@ func UpdateSetDownload2(msg *FilSwan, gid string) (err error) {
 	return
 }
 
-//
-//func UpdateSetDownload3(msg *FilSwan) (err error) {
-//	sqlx := `UPDATE  filswan set  file_active=3  where data_cid='` + msg.DataCid + `'`
-//	log.Debug(sqlx)
-//	if err = orm.Eloquent.Exec(sqlx).Error; err != nil {
-//		return
-//	}
-//	return
-//}
-
 func GetFindOne() (*FilSwan, error) {
 	sk := FilSwan{}
 	sqlx := `select * from filswan where file_active=0   limit 0,1`
@@ -64,16 +54,6 @@ func GetFindOne() (*FilSwan, error) {
 	}
 	return &sk, nil
 }
-
-//func GetFindTwo() (*FilSwan, error) {
-//	sk := FilSwan{}
-//	sqlx := `select * from filswan where file_active=2  ANd limit 0,1`
-//	log.Debug(sqlx)
-//	if err := orm.Eloquent.Raw(sqlx).Scan(&sk).Error; err != nil {
-//		return nil, nil
-//	}
-//	return &sk, nil
-//}
 
 func GeTGId() (ret []*FilSwan, err error) {
 	sqlx := `select  * from  filswan where file_active=1  AND  locked=1 `
