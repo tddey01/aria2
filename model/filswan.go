5fa3e87fac4467ed68b7ce370eb444f796845175
kn <kn@gmail.com>
2022-07-29 17:13:04 +0800

    基础提交 14次

--- model/filswan.go
+++ model/filswan.go
@@ -26,7 +26,7 @@ type FilSwan struct {
 
 func GetAll() (ret []*FilSwan, err error) {
 	table := config.GetConfig().Mysql.Table
-	sqlx := `select  * from  ` + table + ` where file_active=0  limit 0,3`
+	sqlx := `select  * from  ` + table + ` where file_active=0  limit 0,1`
 	log.Debug(sqlx)
 	if err = orm.Eloquent.Raw(sqlx).Scan(&ret).Error; err != nil {
 		return
