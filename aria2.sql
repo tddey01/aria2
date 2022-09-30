ec2aa1e453eea559a89363b803086348b7cf00ec
tddey01 <46470630+tddey01@users.noreply.github.com>
2022-09-30 11:16:39 +0800

    Update aria2.sql

--- aria2.sql
+++ aria2.sql
@@ -1,1 +1,27 @@
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for download
+-- ----------------------------
+DROP TABLE IF EXISTS `download`;
+CREATE TABLE `download`  (
+  `mdi` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `file_size` double NULL DEFAULT 0,
+  `download_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `file_active` int(11) NULL DEFAULT 0,
+  `locked` int(11) NULL DEFAULT 0,
+  `gid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_times` datetime(0) NULL DEFAULT NULL,
+  `update_times` datetime(0) NULL DEFAULT NULL,
+  `local_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `success` int(11) NULL DEFAULT 0,
+  `disk_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `drive` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`mdi`) USING BTREE,
+  UNIQUE INDEX `download_mdi_uindex`(`mdi`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 82 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
